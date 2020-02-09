7fb934b1963c8c620891d1b40c10256ea2536c16
Celestine <celesclaks@yahoo.com>
2020-02-08 21:25:58 -1200

    still some fixes on the dropdown menu

--- osf-project/src/App.js
+++ osf-project/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavBar from './Components/Header/NavBar'
+import DropMenu from './Components/Header/DropMenu'
+import MenuList from './Components/Header/MenuList'
+
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App" style={{maxWidth: 1280}}>
       <NavBar />
     </div>
   );
--- osf-project/src/Components/Header/NavBar.js
+++ osf-project/src/Components/Header/NavBar.js
@@ -4,20 +4,19 @@ import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import DropdownButton from "react-bootstrap/DropdownButton";
-import Dropdown from 'react-bootstrap/Dropdown';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar'
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import "./Header.scss";
 import logo from "../../Images/logo.png";
-import LanguageSelect from './LanguageSelect'
-
+import MenuList from './MenuList'
+import LanguageSelect from "./LanguageSelect";
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
     backgroundColor: "#FFFFFF",
-    display: 'flex'
+    display: "flex"
   },
   menuButton: {
     marginRight: theme.spacing(2)
@@ -41,17 +40,201 @@ export default function ButtonAppBar() {
         </div>
         <Navbar variant="light" className="navMenu">
           <Nav className="mr-auto">
-            <DropdownButton id="dropdown-basic-button" title="SERVICES">
-              <Dropdown.Item href="#/action-1" className="menuLink">
-                Action
-              </Dropdown.Item>
-              <Dropdown.Item href="#/action-2" className="menuLink">
-                Another action
-              </Dropdown.Item>
-              <Dropdown.Item href="#/action-3" className="menuLink">
-                Something else
-              </Dropdown.Item>
-            </DropdownButton>
+            {/*<DropdownButton id="dropdown-basic-button" title="SERVICES">
+              <div className="dropContainer">
+                <div className="dropRow">
+                  <ul>
+                    <li className="dropList">
+                      <a href="#" className="dropLink heading">
+                        product categories
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        Accessories
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        alcohol
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        art
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        books
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        drink
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        electronics
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        flowers & Plans
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        food
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="dropRow">
+                  <ul>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        gadgets
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        garden
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        grocery
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        home
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        jewelry
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        kids & Baby
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        men's Fashion
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        mobile
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="dropRow">
+                  <ul>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        motorcycles
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        movies
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        music
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        office
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        pets
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        romantic
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        sport
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        toys
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="dropRow">
+                  <ul>
+                    <li className="dropList">
+                      <a href="#" className="dropLink heading">
+                        sales
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        Accessories
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        alcohol
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        art
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        books
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        drink
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        electronics
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        flowers & Plans
+                      </a>
+                    </li>
+                    <li className="dropList">
+                      <a href="#" className="dropLink">
+                        food
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <img src={dropImg} className='dropImg' alt="image of lady and shopping bags" />
+  </div>
+            </DropdownButton>*/}
+            <MenuList/>
             <Nav.Link href="#home" className="NavLink">
               COMPANY
             </Nav.Link>
