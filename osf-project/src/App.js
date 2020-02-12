a7e7ae975614b23f9a27538946df7c8217c64270
Celestine <celesclaks@yahoo.com>
2020-02-11 13:56:38 -1200

    Implemented the Products section

--- osf-project/src/App.js
+++ osf-project/src/App.js
@@ -4,6 +4,7 @@ import NavBar from './Components/Header/NavBar'
 import Slider  from './Components/Main/WithCloud/Slider'
 import DropMenu from './Components/Header/DropMenu'
 import MenuList from './Components/Header/MenuList'
+import PopularCard from './Components/Main/PopularItems/PopularCard'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <div className="App" style={{maxWidth: 1280}}>
       <NavBar />
       <Slider />
+      <PopularCard/>
     </div>
   );
 } 
---
+++ osf-project/src/Components/Main/PopularItems/PopularItems.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class PopularItems extends Component {
+    render() {
+        return (
+            <div >
+                <h3 className='PopularItems-head'>Popular Items</h3>
+                
+                
+            </div>
+        )
+    }
+}
+
+export default PopularItems
